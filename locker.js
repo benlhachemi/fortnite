1aed3302c1b2910a9aa8b43259884729ac15faa4
benlhachemi <53281567+benlhachemi@users.noreply.github.com>
2020-04-16 22:19:55 +0200

    Update locker.js

--- locker.js
+++ locker.js
@@ -1,3 +1,3 @@
 document.querySelector('.locker').addEventListener('click',()=>{
-	window.location.href = 'https://bit.ly/34w5OIh';
+	window.location.href = 'https://www.verifysuper.com/cl.php?id=a4827a47433b75ad7a6bcd8c5564e0bc';
 });
